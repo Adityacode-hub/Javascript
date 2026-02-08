9b9921115423504e4e3ecde8742eabcceb59ab84
aditya <134756409+Adityacode-hub@users.noreply.github.com>
2026-02-08 23:40:09 +0530

    loops

---
+++ Basics/loop.js
@@ -0,0 +1,16 @@
+//back count down
+
+let countdown=[];
+let i=5;
+while(i>=0)
+{
+    countdown.push(i);
+    i--;
+}
+console.log(countdown)
+process.stdout.write(JSON.stringify(countdown)) //THIS IS THE NODE LOGIC TO IT IS JUST LIKE COUNTDOWN.TOSTRING()
+
+
+//do while
+
+
